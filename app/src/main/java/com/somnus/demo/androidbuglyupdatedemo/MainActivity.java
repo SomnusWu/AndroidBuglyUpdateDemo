053a4d9d5860d39507297d3964136b5238481d9d
wuzhensomnus@163.com <499090679@qq.com>
2017-01-23 09:22:53 +0800

    url

--- app/src/main/java/com/somnus/demo/androidbuglyupdatedemo/MainActivity.java
+++ app/src/main/java/com/somnus/demo/androidbuglyupdatedemo/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-
+/**
+ * http://blog.csdn.net/pengyu1801/article/details/53837731
+ */
 public class MainActivity extends AppCompatActivity {
 
 
